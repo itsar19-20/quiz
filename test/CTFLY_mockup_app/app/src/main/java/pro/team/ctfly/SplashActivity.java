adadab9ccdfb5ac08e561ea7998c9631d111aa18
davide1110 <davidegrasso200068@gmail.com>
2020-01-16 17:07:35 +0100

    Aggiunto mockup android

---
+++ test/CTFLY_mockup_app/app/src/main/java/pro/team/ctfly/SplashActivity.java
@@ -0,0 +1,26 @@
+package pro.team.ctfly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
+            }
+        }, 2000);
+    }
+}
