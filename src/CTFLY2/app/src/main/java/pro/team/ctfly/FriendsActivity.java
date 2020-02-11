52846643ebc60ed9515e4c63c74ca8acc5dda4fb
davide1110 <davidegrasso200068@gmail.com>
2020-02-11 16:37:15 +0100

    Aggiunto CTFLY2

---
+++ src/CTFLY2/app/src/main/java/pro/team/ctfly/FriendsActivity.java
@@ -0,0 +1,22 @@
+package pro.team.ctfly;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+    }
+
+}
---
+++ src/CTFLY2/app/src/main/java/pro/team/ctfly/LoginActivity.java
@@ -0,0 +1,26 @@
+package pro.team.ctfly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn = findViewById(R.id.signup_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
