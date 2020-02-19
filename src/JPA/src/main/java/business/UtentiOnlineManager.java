9fa98c5b1eb35d60ee51112007b5fa09fc58adcd
Matteo Conti <matteo.conti7@gmail.com>
2020-02-19 09:14:40 +0100

    commit qualcosa

--- src/JPA/src/main/java/business/UtentiOnlineManager.java
+++ src/JPA/src/main/java/business/UtentiOnlineManager.java
@@ -10,14 +10,8 @@ import utility.JPAUtil;
 
 public class UtentiOnlineManager {
 
-Utente uw=new Utente();
 	public List<Utente> selezionaUtenti() {
-		EntityManager em=JPAUtil.getInstance().getEmf().createEntityManager();
-		if (uw.isAttivo()) {
-			List<Utente> list=  em.createQuery("SELECT uw FROM Utente uw WHERE uw.attivo=true", Utente.class).getResultList();
-			Integer num=list.size();	
-		}
-	
+		EntityManager em=JPAUtil.getInstance().getEmf().createEntityManager();	
 		return em.createQuery("SELECT uw FROM Utente uw  WHERE uw.attivo=true", Utente.class).getResultList();
 	}
 }
