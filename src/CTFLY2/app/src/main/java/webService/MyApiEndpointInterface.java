51e161fa0e3f9a3bfefb09c88a6621effcb04d04
teo-conti <matteo.conti7@gmail.com>
2020-03-20 14:07:56 +0100

    commit

--- src/CTFLY2/app/src/main/java/webService/MyApiEndpointInterface.java
+++ src/CTFLY2/app/src/main/java/webService/MyApiEndpointInterface.java
@@ -1,11 +1,9 @@
 package webService;
 import java.util.List;
 
+import Model.Utente;
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 
